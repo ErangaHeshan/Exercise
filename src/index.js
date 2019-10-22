90b0b98da6f3fce0d1a7102d94c91942280ef334
Eranga Heshan <ErangaHeshan@users.noreply.github.com>
2019-10-23 00:44:36 +0530

    Add missing strict equality operator

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const promises = [];
 
 for (let i = 1; i < 101; i++) {
     promises.push(new Promise(resolve => {
-        if (i % 3 === 0 && i % 5 == 0) {
+        if (i % 3 === 0 && i % 5 === 0) {
             resolve(`${i}: FizzBuzz`);
         } else if(i % 3 === 0){
             resolve(`${i}: Fizz`);

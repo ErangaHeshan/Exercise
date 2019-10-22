e1c054ee473bc3781412377e8f9db18068d6d5ee
ErangaHeshan <eranga.h.n@gmail.com>
2019-10-22 23:14:47 +0530

    Implement task-3.1

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ const { getRandomWordSync, getRandomWord } = require('word-maker');
 console.log('It works!');
 
 // YOUR CODE HERE
-for(let i=1; i<101; i++){
-    console.log(`${i}: ${getRandomWordSync()}`);
+const promises = [];
+
+for (let i = 1; i < 101; i++) {
+    promises.push(new Promise(resolve => getRandomWord().then(word => resolve(`${i}: ${word}`))));
 }
+
+Promise.all(promises).then(response => {
+    response.forEach(line => {
+        console.log(line);
+    })
+});

212a3eb922e99ab5fa16f7ce76a031e6dd55894b
Eranga Heshan <ehes1434@corp.sysco.com>
2019-10-22 22:11:13 +0530

    Implement task-1

--- src/index.js
+++ src/index.js
@@ -3,3 +3,6 @@ const { getRandomWordSync, getRandomWord } = require('word-maker');
 console.log('It works!');
 
 // YOUR CODE HERE
+for(let i=1; i<101; i++){
+    console.log(`${i}: ${getRandomWordSync()}`);
+}
